81046b67d367f6ac843a2436d90c1a388f703c1a
Greene <GREENEJ3@legal.regn.net>
2016-10-10 19:46:17 +0100

    housekeeping

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pythonTest',
+      version='0.1',
+      description='just a testbed for learning python',
+      url='https://github.com/jwgreene1/pythonTest.git',
+      author='Josh Greene',
+      author_email='jwgreene1@gmail.com',
+      license='None',
+      packages=['pythonTest'],
+      zip_safe=False)
